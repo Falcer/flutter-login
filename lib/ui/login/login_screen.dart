3d07944e7d74adeebf98421dbaad2547f7c78ee6
arganaphangquestian <arganaphangquestian@gmail.com>
2020-09-26 16:35:30 +0700

    Login Done

--- lib/ui/login/login_screen.dart
+++ lib/ui/login/login_screen.dart
@@ -9,9 +9,10 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-
-  final usernameController = TextEditingController(); // Get Value of Username TextField
-  final passwordController = TextEditingController(); // Get Value of Password TextField
+  final usernameController =
+      TextEditingController(); // Get Value of Username TextField
+  final passwordController =
+      TextEditingController(); // Get Value of Password TextField
 
   @override
   void dispose() {
@@ -23,9 +24,78 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Center(
-          child: Text("Login Screen"),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                controller: usernameController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Username",
+                  hintText: "falcerdev",
+                ),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              TextField(
+                controller: passwordController,
+                obscureText: true,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Password",
+                  hintText: "********",
+                ),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              GestureDetector(
+                onTap: () {},
+                child: Container(
+                  padding: EdgeInsets.symmetric(
+                    vertical: 16,
+                    horizontal: 24,
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Center(
+                    child: Text(
+                      "Login",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Text("or"),
+              SizedBox(
+                height: 16,
+              ),
+              GestureDetector(
+                onTap: () {
+                  Navigator.pushNamed(context, "/register");
+                },
+                child: Container(
+                  child: Center(
+                    child: Text(
+                      "Sign up",
+                      style: TextStyle(color: Colors.blue.withAlpha(90)),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
